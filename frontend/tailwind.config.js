351c73eceb446d22cc79b2362f90dc56d40e5256
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2025-08-15 18:41:08 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 6s infinite',
+        'bounce-slower': 'bounce 8s infinite',
+        'bounce-slowest': 'bounce 10s infinite',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-15px)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
